e7c52c63440799e84b4348d6f5f42af8239a6419
minalcode <minalpatil762@gmail.com>
2026-08-17 22:02:37 +0530

    polymorphism assignment by minal

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,31 +1,26 @@
 package com.driver;
 
 public class Main {
-    public static class product{
-        public int product(int x,int y){
-            return x*y;
+    // Change 'product' to 'Product' with a capital 'P'
+    public static class Product {
+        public int product(int x, int y) {
+            return x * y;
         }
 
-
-
-        public int product(int x,int y,int z){
-            return x*y*z;
+        public int product(int x, int y, int z) {
+            return x * y * z;
         }
 
-
-        public double product(double x,double y){
-            return x*y;
+        public double product(double x, double y) {
+            return x * y;
         }
-
-
-
     }
 
     public static void main(String[] args) {
-        product p = new product();
-        System.out.println(p.product(5,3));
-        System.out.println(p.product(5,3,8));
-        System.out.println(p.product(5.25,3.55));
+        // Change 'product' to 'Product' here as well
+        Product p = new Product();
+        System.out.println(p.product(5, 3));
+        System.out.println(p.product(5, 3, 8));
+        System.out.println(p.product(5.25, 3.55));
     }
-
 }
