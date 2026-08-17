3d6aaf9692b7eccfc709ed38f99affc014621dbe
MayurCode2 <mpmayur2251998@gmail.com>
2026-08-17 12:52:36 +0530

    Polymorphism assignment by minal

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
+    public static class product{
+        public int product(int x,int y){
+            return x*y;
+        }
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public double product(double x,double y){
+            return x*y;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        product p = new product();
+        System.out.println(p.product(5,3));
+        System.out.println(p.product(5,3,8));
+        System.out.println(p.product(5.25,3.55));
+    }
 
 }
