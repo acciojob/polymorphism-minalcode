172ad025d3bcd7615216f23c40d3861903b62912
minalcode <minalpatil762@gmail.com>
2026-08-17 21:57:36 +0530

    polymorphism assignment by minal

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,12 +5,19 @@ public class Main {
         public int product(int x,int y){
             return x*y;
         }
+
+
+
         public int product(int x,int y,int z){
             return x*y*z;
         }
+
+
         public double product(double x,double y){
             return x*y;
         }
+
+
 
     }
 
